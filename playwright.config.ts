62a6270d6e31f858c3fe1b03c48bcf69014610ef
Al-Amin <alamincse12@gmail.com>
2024-12-23 14:07:33 +0600

    updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     baseURL: '',
-    headless:false,
+    headless:true,
     video:'retain-on-failure',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- tests/007Product.spec.ts
+++ tests/007Product.spec.ts
@@ -8,33 +8,33 @@ const users_data=[
   ["Raselz","Alhmedz",faker.internet.email()],
   ["Rosella","Alameda",faker.internet.email()]
 ]
-users_data.forEach(data=>{
-  test(`create user ${data[0]}`, async ({page}) => {
-      await page.goto('https://demo.nopcommerce.com/');
-      await page.getByRole('link', { name: 'Register' }).click();
-      await page.getByLabel('Male', { exact: true }).check();
-      await page.getByLabel('Female').check();
-      await page.getByLabel('First name:').click();
-      await page.getByLabel('First name:').fill(data[0]);
-      await page.getByLabel('Last name:').click();
-      await page.getByLabel('Last name:').fill(data[1]);
-      await page.locator('select[name="DateOfBirthDay"]').selectOption('17');
-      await page.locator('select[name="DateOfBirthMonth"]').selectOption('3');
-      await page.locator('select[name="DateOfBirthYear"]').selectOption('1929');
-      await page.getByLabel('Email:').click();
-      await page.getByLabel('Email:').fill(data[2]);
-      await page.getByLabel('Company name:').click();
-      await page.getByLabel('Company name:').fill('BS-23');
-      await page.getByLabel('Newsletter:').uncheck();
-      await page.getByLabel('Password:', { exact: true }).click();
-      await page.getByLabel('Password:', { exact: true }).fill('@tania12');
-      await page.getByLabel('Confirm password:').click();
-      await page.getByLabel('Confirm password:').fill('@tania12');
-      await page.getByRole('button', { name: 'Register' }).click();
-      await expect(page.locator('body')).toContainText('Your registration completed');
-      await delay(2000);
-  });
-});
+// users_data.forEach(data=>{
+//   test(`create user ${data[0]}`, async ({page}) => {
+//       await page.goto('https://demo.nopcommerce.com/');
+//       await page.getByRole('link', { name: 'Register' }).click();
+//       await page.getByLabel('Male', { exact: true }).check();
+//       await page.getByLabel('Female').check();
+//       await page.getByLabel('First name:').click();
+//       await page.getByLabel('First name:').fill(data[0]);
+//       await page.getByLabel('Last name:').click();
+//       await page.getByLabel('Last name:').fill(data[1]);
+//       await page.locator('select[name="DateOfBirthDay"]').selectOption('17');
+//       await page.locator('select[name="DateOfBirthMonth"]').selectOption('3');
+//       await page.locator('select[name="DateOfBirthYear"]').selectOption('1929');
+//       await page.getByLabel('Email:').click();
+//       await page.getByLabel('Email:').fill(data[2]);
+//       await page.getByLabel('Company name:').click();
+//       await page.getByLabel('Company name:').fill('BS-23');
+//       await page.getByLabel('Newsletter:').uncheck();
+//       await page.getByLabel('Password:', { exact: true }).click();
+//       await page.getByLabel('Password:', { exact: true }).fill('@tania12');
+//       await page.getByLabel('Confirm password:').click();
+//       await page.getByLabel('Confirm password:').fill('@tania12');
+//       await page.getByRole('button', { name: 'Register' }).click();
+//       await expect(page.locator('body')).toContainText('Your registration completed');
+//       await delay(2000);
+//   });
+// });
 const test_data=JSON.parse(JSON.stringify(require("../testdata/testdata.json")));
 test('Login with valid credentials',async({page}) => {
   await page.goto("https://demo.applitools.com/index.html");
